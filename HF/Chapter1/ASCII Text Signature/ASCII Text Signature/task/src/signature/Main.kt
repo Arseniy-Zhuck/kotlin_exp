30a5aad2c293f167fd7887485abbc0407e6ee2e8
Arseniy-Zhuck <arseniyzhuck@mail.ru>
2021-01-18 08:28:07 +0300

    JBA_lesson2

--- HF/Chapter1/ASCII Text Signature/ASCII Text Signature/task/src/signature/Main.kt
+++ HF/Chapter1/ASCII Text Signature/ASCII Text Signature/task/src/signature/Main.kt
@@ -1,13 +1,12 @@
 package signature
 
 fun main() {
-    var s = readLine()!!
-    var l = s.length()
-    for (i in 1..(l+4))
+    val s = readLine()!!
+    val l = s.length
+    for (i in 1..l+4)
         print("*")
     println()
-    println("* " + s + " *")
-    for (i in 1..(l+4))
+    println("* " + s + " *")    
+    for (i in 1..l+4)
         print("*")
-
 }
